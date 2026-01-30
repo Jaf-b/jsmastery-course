eec282bd3c40b004a2a4fdc4807f0e9e72e83b51
Jaf-b <jafredtshikaya@gmail.com>
2026-01-30 01:42:51 +0100

    initialize app

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+        <h1 className="text-5xl font-bold underline ">Welcome to next Js</h1>
+    </div>
+  );
+}
