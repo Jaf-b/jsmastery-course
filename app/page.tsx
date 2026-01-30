314bf123b31f12c1df366638658217ece9635672
Jaf-b <jafredtshikaya@gmail.com>
2026-01-30 14:53:16 +0100

    creating the home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,24 @@
-import Image from "next/image";
+import ExploreBtn from "@/components/ExploreBtn";
+import EventCard from "@/components/EventCard";
+import { events } from "@/lib/constants";
 
 export default function Home() {
   return (
-    <div>
-        <h1 className="text-5xl font-bold underline ">Welcome to next Js</h1>
-    </div>
+    <section>
+        <h1 className="text-center">The Hub for Every Dev <br/> Event You can't Miss</h1>
+        <p className="text-center mt-5">Hackathons, Mettups, and Conferences, All in One Place</p>
+
+        <ExploreBtn/>
+        <div className="mt-20 space-y-7">
+            <h3>Featured Events</h3>
+            <ul className="events">
+                {events.map((event) => (
+                    <li className="list-none" key={event.title}>
+                        <EventCard {...event}/>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    </section>
   );
 }
